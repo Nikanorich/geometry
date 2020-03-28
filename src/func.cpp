4e38751f3df7e52fe25f81a4d708c438c008a11c
Nikanorich <nikanorich5979@gmail.com>
2020-03-28 21:10:12 +0700

    Format code

--- src/func.cpp
+++ src/func.cpp
@@ -1,5 +1,5 @@
-#include <bits/stdc++.h>
 #include "func.h"
+#include <bits/stdc++.h>
 using namespace std;
 float P = 3.141592653589793238466;
 void triangle(float x1, float x2, float x3, float y1, float y2, float y3)
@@ -21,10 +21,10 @@ int menu()
 }
 void circle(int x, int y, int r)
 {
-	float Per, S;
-	cout << "circle(" << x << ", " << y << ", " << r << ")" << endl;
-	Per = 2 * P * r;
-	S = P * pow(r, 2);
-	cout << "S = " << S << endl;
-	cout << "P = " << Per;
+    float Per, S;
+    cout << "circle(" << x << ", " << y << ", " << r << ")" << endl;
+    Per = 2 * P * r;
+    S = P * pow(r, 2);
+    cout << "S = " << S << endl;
+    cout << "P = " << Per;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,21 @@
-#include <bits/stdc++.h>
 #include "func.h"
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     int x, y, r;
     int x1, x2, y1, y2, x3, y3;
     int Var = menu();
-    switch (Var) 
-{
-    	case 1: 
-		{
-        	cin >> x >> y >> r;
-        	circle(x, y, r);
-        	break;
-    	}
-    	case 2: 
-		{
-        	cin >> x1 >> x2 >> x3 >> y1 >> y2 >> y3;
-        	triangle(x1, x2, x3, y1, y2, y3);
-        	break;
-    	}
-}
+    switch (Var) {
+    case 1: {
+        cin >> x >> y >> r;
+        circle(x, y, r);
+        break;
+    }
+    case 2: {
+        cin >> x1 >> x2 >> x3 >> y1 >> y2 >> y3;
+        triangle(x1, x2, x3, y1, y2, y3);
+        break;
+    }
+    }
 }
